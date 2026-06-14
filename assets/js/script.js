9c03b3035ba040f33016adf2ae99c9ccd12adf32
Kat <admin@krh1.co.uk>
2026-06-14 11:33:43 +0100

    set up directories

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var mainContentEl = document.querySelector(".main-content");
+var sideBarEl = document.querySelector(".sidebar");
+
+// create a new div element
+var newDivEl = document.createElement("div");
+newDivEl.className = "widget";
+
+// Create the Heading 2 and Paragraph elements
+var newH2El = document.createElement("h2");
+newH2El.textContent = "New Widget Title";
+
+var newPEl = document.createElement("p");
+newPEl.textContent = "This is the text of the new widget";
+
+// append the new elements to the new div
+newDivEl.appendChild(newH2El);
+newDivEl.appendChild(newPEl);
+
+// append the new div to the main content
+mainContentEl.appendChild(newDivEl);
+
+// Create a new div element for the sidebar
+var newSideBarDivEl = document.createElement("div");
+newSideBarDivEl.className = "sidebar-widget"; 
+
+newDivEl.outerHTML = someHTML;
